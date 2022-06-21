fb06c49798efbe929b22d9e921d7c110ccdd1e63
Sergey Makhlenko <mahlenko-weblive@yandex.ru>
2022-06-22 01:22:11 +0300

    Добавление тарифа по-умолчанию

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
+use Blackshot\CoinMarketSdk\Models\TariffModel;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -64,10 +65,13 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        $tariff_default = TariffModel::where('default', true)->first();
+
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'tariff_id' => $tariff_default->id
         ]);
     }
 }
