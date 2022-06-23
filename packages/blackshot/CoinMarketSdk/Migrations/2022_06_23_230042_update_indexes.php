b4d3a39a79b9b59a3a2bae95734ff05cca8e4089
Sergey Makhlenko <mahlenko-weblive@yandex.ru>
2022-06-23 23:20:55 +0300

    Обновление ключей БД

--- packages/blackshot/CoinMarketSdk/Migrations/2022_06_23_230042_update_indexes.php
+++ packages/blackshot/CoinMarketSdk/Migrations/2022_06_23_230042_update_indexes.php
@@ -31,14 +31,14 @@ class UpdateIndexes extends Migration
      */
     public function down()
     {
-//        Schema::table('signals', function(Blueprint $table) {
-//            $table->dropIndex(['coin_uuid_date']);
-//            $table->index(['date']);
-//        });
-//
-//        Schema::table('coin_quotes', function(Blueprint $table) {
-//            $table->dropIndex(['coin_uuid', 'last_updated']);
-//            $table->index(['last_updated']);
-//        });
+        Schema::table('signals', function(Blueprint $table) {
+            $table->dropIndex(['coin_uuid_date']);
+            $table->index(['date']);
+        });
+
+        Schema::table('coin_quotes', function(Blueprint $table) {
+            $table->dropIndex(['coin_uuid', 'last_updated']);
+            $table->index(['last_updated']);
+        });
     }
 }
